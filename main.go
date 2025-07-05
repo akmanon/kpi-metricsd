de0efcafa294283cde152a7125b1dfc86bbef5f4
akmanon <asis97@DESKTOP-8GJFNUG.>
2025-07-05 07:19:21 -0700

    Refactor TailAndRedirect implementation and enhance tests for file watching and redirection

--- main.go
+++ main.go
@@ -1,39 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"log"
-	"os"
-	"time"
 )
 
 func main() {
+	//f, _ := os.Create("test_log/app.log")
+	//f.Close()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	t := NewTailAndRedirect("test_log/app.log", "test_log/app_redirect.log", ctx)
-	go func() {
-		if err := t.Start(); err != nil {
-			os.Stderr.WriteString("Error starting tail: " + err.Error() + "\n")
-			return
-		}
-	}()
-
-	go func() {
-		f, err := os.OpenFile("test_log/app.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		writer := bufio.NewWriter(f)
-		for {
-			for i := range 10000 {
-				writer.WriteString(fmt.Sprintf("%d, this is test log\n", i))
-			}
-			time.Sleep(500 * time.Millisecond)
-		}
-	}()
-
-	select {}
-
+	t.Start()
 }
